a720a81b51045a5c00ab83f17657234f8700df33
Oodbirds <90927949+Oodbirds@users.noreply.github.com>
2022-09-22 15:21:11 -0400

    final code hw3

--- homework-materials/HW3/solution-hw3/js/price.js
+++ homework-materials/HW3/solution-hw3/js/price.js
@@ -1,8 +1,12 @@
-let glazeSelect = document.getElementById("glazingOptions");
-let packSelect = document.getElementById("pack-size");
+// global variable declaration
+const glazeSelect = document.getElementById("glazingOptions");
+const packSelect = document.getElementById("pack-size");
 let price = document.getElementById("total-price");
 let currentGlaze = 0.00;
 let currentPack = 1.00;
+
+/*create glazing and pack size classes (classes contain name 
+of option and price)*/
 class Glazing{
     
     constructor(name,price){
@@ -18,20 +22,23 @@ class PackSize{
         this.cinnamonRollQuantity = quantity;
     }
 }
-  
+
+//Declaration of galzing and pack option objects
 const keepOriginal = new Glazing("Keep Original",0)
-const sugarMilk = new Glazing("Suage Milk",0)
+const sugarMilk = new Glazing("Sugar Milk",0)
 const vanillaMilk = new Glazing("Vanilla Milk",0.5)
 const doubleChocolate = new Glazing("Double Chocolate",1)
 
 const one = new PackSize ("1",1)
 const three = new PackSize ("3",3)
-const six = new PackSize ("5",5)
-const twelve = new PackSize("10",10)
+const six = new PackSize ("6",5)
+const twelve = new PackSize("12",10)
 
+//Array used to populate glazing and packsize options 
 let glazingElmnts = [keepOriginal,sugarMilk,vanillaMilk,doubleChocolate];
 let packSizeElmnts = [one, three, six, twelve];
 
+//Forloop populates selection options by cycling through the list
 for (let i = 0; i < glazingElmnts.length; i++) {
     let glazeOptn = glazingElmnts[i];
     let optn= document.createElement("option");
@@ -47,6 +54,7 @@ for (let i = 0; i < packSizeElmnts.length; i++) {
     packSelect.appendChild(optn)
 }
 
+//glazing and pack change functions called when new val is selected
 function glazingChange(element) {
     // get value of selected glazing option
     const priceChange = element.value
@@ -59,8 +67,10 @@ function packChange(element) {
     currentPack = Number(priceChange);
     upDatePrice();
 }
+
+//function to calculate new price
 function upDatePrice (){
-    let total = (2.49 + currentGlaze)*currentPack;
+    let total = Math.round(((2.49 + currentGlaze)*currentPack)*100)/100;
     console.log(currentGlaze);
     console.log(currentPack);
     console.log(total);
